4c70ca50f91f5808e6f41ec7b60348a09897386d
marcio <marcio.tadeu2@gmail.com>
2020-11-19 22:17:03 -0300

    Exibindo repositório

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/container/index';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
@@ -67,9 +68,9 @@ export default class Main extends Component {
                 </Form>
 
                 <List>
-                    {repositories.map(repostory => (
-                        <li key={repostory.name}>
-                            <span>{repostory.name}</span>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
                             <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
                         </li>
                     ))}
